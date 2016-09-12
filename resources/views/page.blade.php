d05e749b20eb6a211ad5c0ed5437857b84c0f740
alexa0503 <lori.w@live.cn>
2016-09-12 16:07:59 +0800

    样式修改

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -2,10 +2,8 @@
 @section('content')
 <div class="container">
     <div class="row page">
-        <div class="col-xs-offset-1 col-xs-10 col-md-10 col-md-offset-0">
+        <div class="col-xs-offset-0 col-xs-12 col-md-10 col-md-offset-0">
             {!! $page->content !!}
-        </div>
-        <div class="col-xs-1 col-md-0">
         </div>
     </div>
 </div>
