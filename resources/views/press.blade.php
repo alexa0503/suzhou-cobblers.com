d09be520577712c67193535318e2ea4542863278
alexa0503 <lori.w@live.cn>
2016-09-22 11:36:21 +0800

    媒体页面修改

--- resources/views/press.blade.php
+++ resources/views/press.blade.php
@@ -2,21 +2,19 @@
 @section('content')
 <div class="container">
     @foreach ($types as $type)
-    <div class="rows"><h4 style="font-weight:normal;">{{$type->title}}</h4></div>
     <div class="row press">
+        <h3 style="font-weight:normal;margin:10px 15px 15px;">{{$type->title}}</h3>
         <div class="col-xs-offset-0 col-xs-3 col-md-3 col-md-offset-0">
-            <img src="{{asset($type->thumb)}}">
+            <div style="width:130px;overflow:hidden;"><img src="{{asset($type->thumb)}}"></div>
         </div>
         <div class="col-xs-9 col-md-9">
             <div class="row">
                 @foreach ($type->subPresses as $press)
                 <div class="press-list">
                     <img src="{{asset($press->thumb)}}" height="108">
-                    <div><p>{{$press->title}}</p></div>
+                    <div><p style="margin:10px 0 20px;">{{$press->title}}</p></div>
                 </div>
                 @endforeach
-
-
             </div>
         </div>
         <div class="clearfix"></div>
