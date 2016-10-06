c3d4ae1d49c2f221c07dbeb1b021c1ab6500b4a3
alexa0503 <lori.w@live.cn>
2016-10-07 02:19:43 +0800

    press页面样式调整

--- resources/views/press.blade.php
+++ resources/views/press.blade.php
@@ -3,12 +3,12 @@
 <div class="container">
     @foreach ($types as $type)
     <div class="row press">
-        <h3 style="font-weight:normal;margin:10px 15px 15px;">{{$type->title}}</h3>
-        <div class="col-xs-offset-0 col-xs-3 col-md-3 col-md-offset-0">
-            <div style="width:130px;overflow:hidden;"><img src="{{asset($type->thumb)}}"></div>
+        <div style="width:164px;text-align:center;" class="pull-left">
+            <img src="{{asset($type->thumb)}}">
+            <h4 style="margin:10px 10px;">{{$type->title}}</h4>
         </div>
-        <div class="col-xs-9 col-md-9">
-            <div class="row">
+        <div style="margin-left:186px;padding-right:60px;">
+            <div class="rows">
                 @foreach ($type->subPresses as $press)
                 <div class="press-list">
                     <img src="{{asset($press->thumb)}}" height="108">
